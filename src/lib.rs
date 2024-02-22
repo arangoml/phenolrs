a40058aa5eb5e19911f054d492e2192532183d60
Alex Geenen <ageenen@pm.me>
2024-02-22 13:29:11 +0100

    Extract Graph Conversion

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,7 @@ mod graphs;
 mod retrieval;
 mod arangodb;
 mod load_request;
+mod data_to_arrays;
 
 use std::collections::HashMap;
 use numpy::{Ix2, ToPyArray, PyArray, IntoPyArray, ndarray};
@@ -10,6 +11,7 @@ use ndarray::prelude::*;
 use pyo3::prelude::*;
 use pyo3::types::PyDict;
 use std::sync::Arc;
+use crate::data_to_arrays::{convert_coo_edge_map, convert_nested_features_map};
 use crate::graphs::Graph;
 
 type PygCompatible<'a> = (&'a PyDict, &'a PyDict, &'a PyDict);
@@ -57,42 +59,8 @@ fn construct_cols_to_keys_to_inds<'pl>(input: HashMap<String, HashMap<String, us
 #[pyfunction]
 fn graph_to_pyg_format<'a>(py: Python<'a>) -> PyResult<PygCompatible<'a>> {
     let graph = retrieval::get_arangodb_graph();
-    let col_to_features = construct_col_to_features(graph.cols_to_features.iter().map(|(col_name, features)| {
-        let mut col_map: HashMap<String, Array2<f64>> = features.iter().filter_map(|(feature_name, nested_feature_vec)| {
-            if nested_feature_vec.is_empty() {
-                return None;
-            }
-            let num_verts: usize = nested_feature_vec.len();
-            let dim =
-                if nested_feature_vec.len() > 0 {
-                    nested_feature_vec[0].len()
-                } else {
-                    0
-                };
-            let mut arr = Array2::<f64>::default((num_verts, dim));
-            for (i, mut row) in arr.axis_iter_mut(Axis(0)).enumerate() {
-                for (j, col) in row.iter_mut().enumerate() {
-                    *col = nested_feature_vec[i][j];
-                }
-            }
-            Some((feature_name.clone(), arr))
-        }).collect();
-        (col_name.clone(), col_map)
-    }).collect(), py)?;
-    let coo_by_from_edge_to = construct_coo_by_from_edge_to(graph.coo_by_from_edge_to.iter().filter_map(|(edge_tup, edge_mat)| {
-        if edge_mat.is_empty() {
-            return None;
-        }
-        let num_edges: usize = edge_mat[0].len();
-        let dim: usize = 2;
-        let mut arr = Array2::<usize>::default((dim, num_edges));
-        for (i, mut row) in arr.axis_iter_mut(Axis(0)).enumerate() {
-            for (j, col) in row.iter_mut().enumerate() {
-                *col = edge_mat[i][j];
-            }
-        }
-        Some((edge_tup.clone(), arr))
-    }).collect(), py)?;
+    let col_to_features = construct_col_to_features(convert_nested_features_map(graph.cols_to_features), py)?;
+    let coo_by_from_edge_to = construct_coo_by_from_edge_to(convert_coo_edge_map(graph.coo_by_from_edge_to), py)?;
     let cols_to_keys_to_inds = construct_cols_to_keys_to_inds(graph.cols_to_keys_to_inds, py)?;
     println!("Finished retrieval!");
     let res = (col_to_features, coo_by_from_edge_to, cols_to_keys_to_inds);
