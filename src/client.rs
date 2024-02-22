82e9e683fe910ce0a979816712ea6ddfadc2cf99
Alex Geenen <ageenen@pm.me>
2024-02-22 18:14:46 +0100

    Flatten Auth

--- src/client.rs
+++ src/client.rs
@@ -59,14 +59,19 @@ pub fn handle_auth(
 ) -> RequestBuilder {
     match &db_config.jwt_token {
         Some(token) => request_builder.bearer_auth(token),
+        None => handle_basic_auth(request_builder, &db_config),
+    }
+}
+
+fn handle_basic_auth(
+    request_builder: RequestBuilder,
+    db_config: &&DatabaseConfiguration,
+) -> RequestBuilder {
+    match &db_config.username {
+        Some(username) => request_builder.basic_auth(username, db_config.password.as_ref()),
         None => {
-            match &db_config.username {
-                Some(username) => request_builder.basic_auth(username, db_config.password.as_ref()),
-                None => {
-                    // proceed without authentication
-                    request_builder
-                }
-            }
+            // proceed without authentication
+            request_builder
         }
     }
 }
