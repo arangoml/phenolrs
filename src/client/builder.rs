d99067a238ef4445ed8b2b62cd63f2cc0a900353
Alex Geenen <ageenen@pm.me>
2024-02-23 19:00:23 +0100

    Refactor Builder

--- src/client/builder.rs
+++ src/client/builder.rs
@@ -1,5 +1,5 @@
 use crate::client::config::ClientConfig;
-use reqwest::Client;
+use reqwest::{Certificate, Client};
 use reqwest_middleware::{ClientBuilder, ClientWithMiddleware};
 use reqwest_retry::policies::ExponentialBackoff;
 use reqwest_retry::RetryTransientMiddleware;
@@ -10,52 +10,39 @@ pub fn build_client(
     config: &ClientConfig,
 ) -> Result<reqwest_middleware::ClientWithMiddleware, String> {
     let mut client_builder = reqwest::Client::builder();
-    if config.use_tls {
+    client_builder = if config.use_tls {
         client_builder = client_builder
             .min_tls_version(reqwest::tls::Version::TLS_1_2)
             .https_only(true);
 
         if let Some(cert_path) = &config.tls_cert {
-            let mut cert_buf = vec![];
-            let file_open = File::open(cert_path);
-            if let Err(err) = file_open {
-                return Err(format!(
-                    "Error message from reading TLS certificate: {:?}",
-                    err
-                ));
-            }
-            let cert_read = file_open.unwrap().read_to_end(&mut cert_buf);
-            if let Err(err) = cert_read {
-                return Err(format!(
-                    "Error message from reading TLS certificate: {:?}",
-                    err
-                ));
-            }
-            let cert = reqwest::Certificate::from_pem(&cert_buf);
-            if let Err(err) = cert {
-                return Err(format!("Error message from request builder: {:?}", err));
-            }
-            client_builder = client_builder
-                .add_root_certificate(cert.unwrap())
-                .use_rustls_tls();
+            let cert = get_cert(cert_path)?;
+            client_builder.add_root_certificate(cert).use_rustls_tls()
+        } else {
+            client_builder
         }
-        let client = client_builder.build();
-        if let Err(err) = client {
-            return Err(format!("Error message from request builder: {:?}", err));
-        }
-        let client = client_with_retries(&config.n_retries, client.unwrap());
-        Ok(client)
     } else {
-        let client = client_builder.build();
-        if let Err(err) = client {
-            return Err(format!("Error message from request builder: {:?}", err));
-        }
-        let client = client_with_retries(&config.n_retries, client.unwrap());
-        Ok(client)
-    }
+        client_builder
+    };
+    let client = client_builder
+        .build()
+        .map_err(|err| format!("Error message from request builder: {:?}", err))?;
+    let client = client_with_retries(&config.n_retries, client);
+    Ok(client)
 }
 
-pub fn client_with_retries(n_retries: &u32, client: Client) -> ClientWithMiddleware {
+fn get_cert(cert_path: &String) -> Result<Certificate, String> {
+    let mut cert_buf = vec![];
+    let mut file = File::open(cert_path)
+        .map_err(|err| format!("Error message from reading TLS certificate: {:?}", err))?;
+    file.read_to_end(&mut cert_buf)
+        .map_err(|err| format!("Error message from reading TLS certificate: {:?}", err))?;
+    let cert = reqwest::Certificate::from_pem(&cert_buf)
+        .map_err(|err| format!("Error message from request builder: {:?}", err))?;
+    Ok(cert)
+}
+
+fn client_with_retries(n_retries: &u32, client: Client) -> ClientWithMiddleware {
     let retry_policy = ExponentialBackoff::builder()
         .retry_bounds(
             std::time::Duration::from_millis(30),
