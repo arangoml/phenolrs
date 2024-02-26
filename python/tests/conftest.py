e700faa0ba9a9ea93250c1a44562989706c81a51
Alex Geenen <ageenen@pm.me>
2024-02-26 11:06:27 +0100

    Python Linting & Formatting

---
+++ python/tests/conftest.py
@@ -0,0 +1,31 @@
+from typing import Any, Dict
+
+import pytest
+
+
+def pytest_addoption(parser: pytest.Parser) -> None:
+    parser.addoption("--url", action="store", default="http://localhost:8529")
+    parser.addoption("--dbName", action="store", default="abide")
+    parser.addoption("--username", action="store", default="root")
+    parser.addoption("--password", action="store", default="test")
+
+
+def pytest_configure(config: pytest.Config) -> None:
+    global connection_config
+    connection_config = {
+        "url": config.getoption("url", default=None),
+        "username": config.getoption("username"),
+        "password": config.getoption("password"),
+        "dbName": config.getoption("dbName"),
+    }
+
+
+@pytest.fixture(scope="session")
+def connection_information() -> Dict[str, Any]:
+    global connection_config
+    return {
+        "url": connection_config.get("url"),
+        "username": connection_config.get("username"),
+        "password": connection_config.get("password"),
+        "dbName": connection_config.get("dbName"),
+    }
