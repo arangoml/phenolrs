fc06c6fd525399c33b1e26a9c8ab407a4a20799c
Alex Geenen <ageenen@pm.me>
2024-02-26 15:31:10 +0100

    Update Python Tests

--- python/tests/conftest.py
+++ python/tests/conftest.py
@@ -1,6 +1,8 @@
 from typing import Any, Dict
 
+import arango
 import pytest
+from arango_datasets import Datasets
 
 connection_config: Dict[str, Any]
 
@@ -31,3 +33,22 @@ def connection_information() -> Dict[str, Any]:
         "password": connection_config.get("password"),
         "dbName": connection_config.get("dbName"),
     }
+
+
+@pytest.fixture(scope="module")
+def load_abide(connection_information: Dict[str, Any]) -> None:
+    client = arango.ArangoClient(connection_information["url"])
+    sys_db = client.db(
+        "_system",
+        username=connection_information["username"],
+        password=connection_information["password"],
+    )
+    sys_db.delete_database("abide", ignore_missing=True)
+    sys_db.create_database("abide")
+    abide_db = client.db(
+        "abide",
+        username=connection_information["username"],
+        password=connection_information["password"],
+    )
+    dsets = Datasets(abide_db)
+    dsets.load("ABIDE")
--- python/tests/test_all.py
+++ python/tests/test_all.py
@@ -3,7 +3,9 @@ from torch_geometric.data import HeteroData
 from phenolrs.pyg_loader import PygLoader
 
 
-def test_phenol_abide_hetero(connection_information: dict[str, str]) -> None:
+def test_phenol_abide_hetero(
+    load_abide: None, connection_information: dict[str, str]
+) -> None:
     result = PygLoader.load_into_pyg_heterodata(
         connection_information["dbName"],
         [{"name": "Subjects", "fields": ["label", "brain_fmri_features"]}],
