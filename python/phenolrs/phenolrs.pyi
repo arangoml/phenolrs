5f4d88e6ba463c8f7ac9f2b5a00f42fe57221cf6
Anthony Mahanna <anthony.mahanna@arangodb.com>
2024-05-01 22:02:52 -0400

    Update phenolrs.pyi

--- python/phenolrs/phenolrs.pyi
+++ python/phenolrs/phenolrs.pyi
@@ -3,11 +3,6 @@ import typing
 import numpy as np
 import numpy.typing as npt
 
-def graph_to_pyg_format(request: dict[str, typing.Any]) -> typing.Tuple[
-    dict[str, dict[str, npt.NDArray[np.float64]]],
-    dict[typing.Tuple[str, str, str], npt.NDArray[np.float64]],
-    dict[str, dict[str, int]],
-]: ...
 def graph_to_json_format(request: dict[str, typing.Any]) -> typing.Tuple[
     dict[str, dict[str, typing.Any]],
     dict[str, dict[str, dict[str, typing.Any]]],
