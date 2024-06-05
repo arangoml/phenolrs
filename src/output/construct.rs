e0c1655451f5074bc8770869b8a0f6a5321fa2be
Anthony Mahanna <anthony.mahanna@arangodb.com>
2024-06-04 20:09:22 -0400

    new: `construct` functions

--- src/output/construct.rs
+++ src/output/construct.rs
@@ -1,8 +1,13 @@
 use ndarray::{Array, Ix2};
 use numpy::ToPyArray;
-use pyo3::types::PyDict;
+use pyo3::types::{PyDict, PyList};
 use pyo3::{PyResult, Python};
 use std::collections::HashMap;
+
+use serde_json::{Map, Value};
+
+#[cfg(not(test))]
+use pyo3::prelude::*;
 
 #[cfg(not(test))] // not(test) is needed to let us use `cargo test`
 pub fn construct_col_to_features(
@@ -54,4 +59,89 @@ pub fn construct_cols_to_inds_to_keys(
         .iter()
         .for_each(|item| dict.set_item(item.0, item.1).unwrap());
     Ok(dict)
+}
+
+#[cfg(not(test))]
+pub fn construct_dict(input: HashMap<String, usize>, py: Python) -> PyResult<&PyDict> {
+    let pydict = PyDict::new(py);
+
+    for (key, value) in input {
+        pydict.set_item(key, value)?;
+    }
+
+    Ok(pydict)
+}
+
+#[cfg(not(test))]
+pub fn construct_dict_of_dict(
+    input: HashMap<String, Map<String, Value>>,
+    py: Python,
+) -> PyResult<&PyDict> {
+    let pydict = PyDict::new(py);
+
+    for (key, properties) in input.iter() {
+        let inner_dict = PyDict::new(py);
+        for (property_key, property_value) in properties {
+            let py_value = construct_py_object(property_value, py)?;
+            inner_dict.set_item(property_key, py_value)?;
+        }
+        pydict.set_item(key, inner_dict)?;
+    }
+
+    Ok(pydict)
+}
+
+#[cfg(not(test))]
+pub fn construct_dict_of_dict_of_dict(
+    input: HashMap<String, HashMap<String, Map<String, Value>>>,
+    py: Python,
+) -> PyResult<&PyDict> {
+    let pydict = PyDict::new(py);
+
+    for (key, properties) in input.iter() {
+        let inner_dict = PyDict::new(py);
+        for (property_key, property_value) in properties.iter() {
+            let inner_inner_dict = PyDict::new(py);
+            for (inner_property_key, inner_property_value) in property_value {
+                let py_value = construct_py_object(inner_property_value, py)?;
+                inner_inner_dict.set_item(inner_property_key, py_value)?;
+            }
+            inner_dict.set_item(property_key, inner_inner_dict)?;
+        }
+        pydict.set_item(key, inner_dict)?;
+    }
+
+    Ok(pydict)
+}
+
+#[cfg(not(test))]
+fn construct_py_object(value: &Value, py: Python) -> PyResult<PyObject> {
+    match value {
+        Value::Null => Ok(py.None()),
+        Value::String(s) => Ok(s.to_object(py)),
+        Value::Bool(b) => Ok(b.to_object(py)),
+        Value::Number(num) => {
+            if let Some(i) = num.as_i64() {
+                Ok(i.to_object(py))
+            } else if let Some(u) = num.as_u64() {
+                Ok(u.to_object(py))
+            } else {
+                Ok(num.as_f64().unwrap().to_object(py))
+            }
+        }
+        Value::Array(arr) => {
+            let py_list = PyList::empty(py);
+            for item in arr {
+                py_list.append(construct_py_object(item, py)?)?;
+            }
+            Ok(py_list.to_object(py))
+        }
+        Value::Object(obj) => {
+            let py_dict = PyDict::new(py);
+            for (key, value) in obj {
+                py_dict.set_item(key, construct_py_object(value, py)?)?;
+            }
+            Ok(py_dict.to_object(py))
+        }
+    }
 }
