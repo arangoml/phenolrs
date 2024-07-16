5a8e63f4c3cf6c3db8e0bcc663505458904d2c3f
Anthony Mahanna <anthony.mahanna@arangodb.com>
2024-07-16 17:30:28 +0200

    update `.pyi`

--- python/phenolrs/phenolrs.pyi
+++ python/phenolrs/phenolrs.pyi
@@ -3,11 +3,21 @@ import typing
 import numpy as np
 import numpy.typing as npt
 
-def graph_to_pyg_format(request: dict[str, typing.Any]) -> typing.Tuple[
+def graph_to_numpy_format(request: dict[str, typing.Any]) -> typing.Tuple[
     dict[str, dict[str, npt.NDArray[np.float64]]],
     dict[typing.Tuple[str, str, str], npt.NDArray[np.float64]],
     dict[str, dict[str, int]],
     dict[str, dict[int, str]],
+]: ...
+
+def graph_to_networkx_format(
+    request: dict[str, typing.Any], graph_config: dict[str, typing.Any]
+) -> typing.Tuple[
+    dict[str, dict[str, typing.Any]],
+    dict[str, dict[str, dict[str, typing.Any] | dict[int, dict[str, typing.Any]]]],
+    npt.NDArray[np.int64],
+    npt.NDArray[np.int64],
+    dict[str, int],
 ]: ...
 
 class PhenolError(Exception): ...
