fd61a57c5c9d41f527aaf80d932ae16bd94f90d4
Anthony Mahanna <anthony.mahanna@arangodb.com>
2024-07-16 17:55:19 +0200

    new: `graph_to_networkx_format`
    (maturin will fail for now)

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,8 @@ mod input;
 mod load;
 mod output;
 
-use input::load_request::DataLoadRequest;
+use input::load_request::{DataLoadRequest, NetworkXGraphConfig};
+use numpy::PyArray1;
 #[cfg(not(test))]
 use output::construct;
 use output::convert::{convert_coo_edge_map, convert_nested_features_map};
@@ -15,6 +16,11 @@ use pyo3::exceptions::PyException;
 use pyo3::prelude::*;
 #[cfg(not(test))]
 use pyo3::types::PyDict;
+
+use graph::{NetworkXGraph, NumpyGraph};
+
+use std::sync::Arc;
+use std::sync::RwLock;
 
 #[cfg(not(test))]
 type PygCompatible<'a> = (&'a PyDict, &'a PyDict, &'a PyDict, &'a PyDict);
@@ -58,12 +64,61 @@ fn graph_to_numpy_format(py: Python, request: DataLoadRequest) -> PyResult<PygCo
     Ok(res)
 }
 
+#[pyfunction]
+#[cfg(not(test))]
+fn graph_to_networkx_format(
+    py: Python,
+    request: DataLoadRequest,
+    graph_config: NetworkXGraphConfig,
+) -> PyResult<(
+    &PyDict,
+    &PyDict,
+    &PyArray1<usize>,
+    &PyArray1<usize>,
+    &PyDict,
+)> {
+    let graph_factory = || {
+        NetworkXGraph::new(
+            graph_config.load_node_dict,
+            graph_config.load_adj_dict,
+            graph_config.load_adj_dict_as_directed,
+            graph_config.load_adj_dict_as_multigraph,
+            graph_config.load_coo,
+        )
+    };
+
+    let graph = load::retrieve::get_arangodb_graph(request, graph_factory).unwrap();
+
+    let node_dict = construct::construct_dict_of_dict(graph.node_map, py)?;
+    let adj_dict = if graph_config.load_adj_dict_as_multigraph {
+        construct::construct_dict_of_dict_of_dict_of_dict(graph.adj_map_multigraph, py)?
+    } else {
+        construct::construct_dict_of_dict_of_dict(graph.adj_map, py)?
+    };
+
+    let coo = graph.coo;
+    let src_indices = PyArray1::from_vec(py, coo.0);
+    let dst_indices = PyArray1::from_vec(py, coo.1);
+    let vertex_id_to_index = construct::construct_dict(graph.vertex_id_to_index, py)?;
+
+    let res = (
+        node_dict,
+        adj_dict,
+        src_indices,
+        dst_indices,
+        vertex_id_to_index,
+    );
+
+    Ok(res)
+}
+
 /// A Python module implemented in Rust.
 #[cfg(not(test))]
 #[pymodule]
 #[cfg(not(test))]
 fn phenolrs(py: Python, m: &PyModule) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(graph_to_numpy_format, m)?)?;
+    m.add_function(wrap_pyfunction!(graph_to_networkx_format, m)?)?;
     m.add("PhenolError", py.get_type::<PhenolError>())?;
     Ok(())
 }
