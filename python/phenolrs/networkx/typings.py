90562366a1ca2a894711adacc4c312988f55370c
Anthony Mahanna <anthony.mahanna@arangodb.com>
2024-08-09 17:41:13 -0400

    remove `bool` as possible `EdgeValue` type
    (my mistake)

--- python/phenolrs/networkx/typings.py
+++ python/phenolrs/networkx/typings.py
@@ -9,7 +9,7 @@ GraphAdjDict = dict[str, dict[str, Json]]
 DiGraphAdjDict = dict[str, GraphAdjDict]
 MultiGraphAdjDict = dict[str, dict[str, dict[int, Json]]]
 MultiDiGraphAdjDict = dict[str, MultiGraphAdjDict]
-EdgeValuesDict = dict[str, list[int | float | bool]]
+EdgeValuesDict = dict[str, list[int | float]]
 
 SrcIndices = npt.NDArray[np.int64]
 DstIndices = npt.NDArray[np.int64]
