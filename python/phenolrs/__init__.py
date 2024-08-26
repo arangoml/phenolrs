734ac997675739ffaaeadef62fd9d2392ff6f4ce
Anthony Mahanna <anthony.mahanna@arangodb.com>
2024-08-26 08:52:07 -0400

    temp: raise value error

--- python/phenolrs/__init__.py
+++ python/phenolrs/__init__.py
@@ -1,5 +1,7 @@
 from .phenolrs import *  # noqa: F403
 
+raise ValueError("temp")
+
 __doc__ = phenolrs.__doc__  # type: ignore[name-defined]  # noqa: F405
 if hasattr(phenolrs, "__all__"):  # type: ignore[name-defined]  # noqa: F405
     __all__ = phenolrs.__all__  # type: ignore[name-defined]  # noqa: F405
