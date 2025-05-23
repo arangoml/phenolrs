1441dc15780be134fcaf862247d487e6825189a1
Anthony Mahanna <anthony.mahanna@arangodb.com>
2025-05-23 15:37:53 -0400

    fix: fmt

--- src/input/parse.rs
+++ src/input/parse.rs
@@ -3,7 +3,7 @@ use arangors_graph_exporter::graph_loader::CollectionInfo;
 use arangors_graph_exporter::{DataLoadConfiguration, DatabaseConfiguration};
 use pyo3::exceptions::PyValueError;
 use pyo3::types::PyDict;
-use pyo3::{FromPyObject, PyAny, PyResult, Bound, prelude::*};
+use pyo3::{prelude::*, Bound, FromPyObject, PyAny, PyResult};
 
 #[derive(Default)]
 pub struct LocalDataLoadConfiguration(pub DataLoadConfiguration);
