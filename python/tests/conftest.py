06d9daaf88e992b2b890caf904e69e24c51079e0
Anthony Mahanna <anthony.mahanna@arangodb.com>
2025-05-23 13:07:04 -0400

    ignore F824

--- python/tests/conftest.py
+++ python/tests/conftest.py
@@ -27,7 +27,7 @@ def pytest_configure(config: pytest.Config) -> None:
 
 @pytest.fixture(scope="session")
 def connection_information() -> Dict[str, Any]:
-    global connection_config  # noqa: F821
+    global connection_config
     return {
         "url": connection_config.get("url"),
         "username": connection_config.get("username"),
